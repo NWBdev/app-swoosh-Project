1122d01a4a201a935feb7150ba92cf705861fbd7
Nathaniel Burciaga <nathanielburciaga@nathaniels-mbp>
2017-12-12 23:57:06 -0800

    Added graphics for UI, Created UIButton Class for borders.

---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,31 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Nathaniel Burciaga on 12/12/17.
+//  Copyright © 2017 Nathaniel Burciaga. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+        
+    }
+    
+    
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
