9f171ceb9d4a597024d0000b666d2b858360310f
Nathaniel Burciaga <nathanielburciaga@nathaniels-mbp>
2017-12-28 02:08:51 -0800

    Segue's added for VC, and added back button UI

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -27,6 +27,20 @@ class ViewController: UIViewController {
          **/
     }
 
+    
+    
+    
+    //Segue
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
